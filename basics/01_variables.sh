38ef15b6e9018bb2519a2ceb1aa7dc0f61b9bda3
Ubuntu <shindepranav12022@gmail.com>
2026-05-11 07:19:16 +0000

    Add variable practice script

---
+++ basics/01_variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#!/bin/bash
+name="pranav"
+age=21
+favourite_language="python"
+today=$(date)
+echo "my name is $name"
+echo "my age is $age"
+echo "my favourite language is $favourite_language"
+echo "today is $today"
+
