ade1fe86fe9fe557f7958f912408beb5fca8b055
Ubuntu <shindepranav12022@gmail.com>
2026-05-11 11:01:18 +0000

    add user input practice script

---
+++ basics/02_user_input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "enter your name: " name
+read -p "enter your city: " city
+read -p "enter your favorite language: " favorite_language
+
+echo "my name is $name"
+echo "i live in $city"
+echo "my favorite language is $favorite_language"
+echo "current logged in user is $USER"
